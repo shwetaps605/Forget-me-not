ed1947af7eea5ee147cf4cdf45dac5e826ef1694
Roy <shweroy@apac.corpdir.net>
2021-11-19 16:22:39 +0530

    Added the barebone structure to run the Spring Boot application

---
+++ src/main/java/com/example/contactslistingapp/ContactsListingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactslistingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsListingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsListingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/contactslistingapp/ContactsListingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contactslistingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsListingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
